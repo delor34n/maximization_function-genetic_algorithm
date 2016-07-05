12bcc7b71cd623aee74b6d96f6c1310bd2ba5323
delor34n <sebastian.lastra@usach.cl>
2016-07-05 00:22:22 -0300

    agrega proyecto en netbeans

---
+++ ga_script.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   ga_script.cpp
+ * Author: macbook
+ *
+ * Created on 4 de julio de 2016, 23:49
+ */
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "hello world!" << endl;
+    return 0;
+}
+
